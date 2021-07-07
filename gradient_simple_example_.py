eea6a8246103f15996bd6ec35a72e4650fdcfe06
Saber <1650025554@qq.com>
2021-07-07 19:17:58 +0800

    simple gradient

---
+++ gradient_simple_example_.py
@@ -0,0 +1,38 @@
+import random
+import time
+
+import matplotlib.pyplot as plt
+import numpy as np
+from icecream import ic
+
+
+def loss(x, w, b, y):
+    return ((w * x + b) - y) ** 2
+
+
+def patial_w(x, w, b, y):
+    return 2 * ((w * x + b) - y) * x
+
+
+def patial_b(x, w, b, y):
+    return 2 * ((w * x + b) - y)
+
+
+w, b = random.randint(-10, 10), random.randint(-10, 10)
+
+x, y = 10, 0.35
+alpha = 1e-3
+for i in range(100):
+    w = w + -1 * patial_w(x, w, b, y)*alpha
+    b -= patial_b(x, w, b, y)
+    ic(loss(x, w, b, y))
+    x_l = np.linspace(0, 13, 100)
+    if i%4 ==0:
+        plt.cla()
+        plt.plot(x_l, w * x_l + b, c='black')
+        plt.scatter(x, y, marker="*", c='red')
+        plt.text(1, 4, 'Loss={0}'.format(loss(x, w, b, y)), fontdict={'size': 10, 'color': 'red'})
+        plt.pause(0.1)
+
+
+
