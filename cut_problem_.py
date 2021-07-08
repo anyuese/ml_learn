3ac1125b4496240a6e4a349f0ab8f18ea0e2f15c
Saber <1650025554@qq.com>
2021-07-08 10:04:17 +0800

    dynamic plan(cut_problem)

---
+++ cut_problem_.py
@@ -0,0 +1,72 @@
+from collections import defaultdict
+from functools import lru_cache
+
+from icecream import ic
+
+prices = [1, 5, 8, 9, 10, 17, 17, 20, 24, 30, 33]
+
+length_prices = defaultdict(int)
+for i, p in enumerate(prices): length_prices[i + 1] = p
+ic(dict(length_prices))
+
+
+# 计算如何切木材的收益最大
+def revnue(n, cache={}):
+    if n in cache: return cache[n]
+    candidates = [length_prices[n]]
+    for i in range(1, n):
+        candidate = revnue(i, cache) + revnue(n - i, cache)
+        candidates.append(candidate)
+    max_profit = max(candidates)
+    cache[n] = max_profit
+    return max_profit
+
+ic(revnue(118))
+
+
+# 计算切木材收益最大,并能提取怎么切
+def revnue_solution(n, solutions={}, cache={}):
+    if n in cache: return cache[n], solutions[n]
+    candidates = [(length_prices[n], (n, 0))]
+    for i in range(1, n):
+        candidate = (revnue_solution(i, solutions, cache)[0] + revnue_solution(n - i, solutions, cache)[0], (i, n - i))
+        candidates.append(candidate)
+    max_profit, solution = max(candidates, key=lambda x: x[0])
+    cache[n] = max_profit
+    solutions[n] = solution
+    return max_profit, solutions
+
+ic(revnue_solution(118))
+ic(revnue_solution(18))
+# ic(revnue_solution(118))
+
+# 解析路径
+def parse_solution(path, n):
+    i, j = path[n]
+    if j == 0:
+        return path[n]
+    else:
+        return parse_solution(path, i) + parse_solution(path, j)
+
+
+# ic(parse_solution(revnue_solution(118)[1], 118))
+# ic(revnue_solution(8))
+SOLUTIONS = {}
+
+
+@lru_cache(maxsize=2 ** 10)
+def revnue_solution_cache(n):
+    candidates = [(length_prices[n], (n, 0))]
+    for i in range(1, n):
+        candidate = (revnue_solution_cache(i) + revnue_solution_cache(n - i), (i, n - i))
+        candidates.append(candidate)
+
+    global SOLUTIONS
+    max_profit, solution = max(candidates, key=lambda x: x[0])
+    SOLUTIONS[n] = solution
+    return max_profit
+
+
+ic(revnue_solution_cache(200))
+ic(SOLUTIONS)
+ic(parse_solution(SOLUTIONS,200))
--- gradient_simple_example_.py
+++ gradient_simple_example_.py
@@ -5,15 +5,15 @@ import matplotlib.pyplot as plt
 import numpy as np
 from icecream import ic
 
-
+# 定义loss 函数
 def loss(x, w, b, y):
     return ((w * x + b) - y) ** 2
 
-
+# 定义w的偏导
 def patial_w(x, w, b, y):
     return 2 * ((w * x + b) - y) * x
 
-
+# 定义b的偏导
 def patial_b(x, w, b, y):
     return 2 * ((w * x + b) - y)
 
@@ -27,6 +27,8 @@ for i in range(100):
     b -= patial_b(x, w, b, y)
     ic(loss(x, w, b, y))
     x_l = np.linspace(0, 13, 100)
+
+    # 可视化查看效果
     if i%4 ==0:
         plt.cla()
         plt.plot(x_l, w * x_l + b, c='black')
